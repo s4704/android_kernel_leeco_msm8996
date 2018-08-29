68a229bec46ea57ca15a9dcf5a62016941ebb11f
s4704 <igorkud00@gmail.com>
2018-08-29 19:15:41 +0300

    cpufreq: electron gov
    Electron is a governor based on interactive from the latest MSM8994 CAF branch with more upstream improvements, powersave bias,
    screen off max frequency, and some other tweaks to improve battery life without hindering performance.
    
    Thanks to Google, CAF, Linaro, imoseyon, neobuddy, and many others.
    
    Signed-off-by: Joe Maples <joe@frap129.org>
    Signed-off-by: Planet-X <planetx.dev@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -540,6 +540,9 @@ extern struct cpufreq_governor cpufreq_gov_zzmoove;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BIOSHOCK)
 extern struct cpufreq_governor cpufreq_gov_bioshock;
 #define CPUFREQ_DEFAULT_GOVERNOR    (&cpufreq_gov_bioshock)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ELECTRON)
+extern struct cpufreq_governor cpufreq_gov_electron;
+#define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_electron)
 #endif
 
 /*********************************************************************
